ebb02d463d4a163c0bdf67db04eea2baab38813e
Matthew Chiam <mchiam@mc.local>
2015-03-17 16:27:21 +1300

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("matthew", 23);
+            Console.WriteLine("Hello " + person.Name + "!");
+
+            Console.Read();
+        }
+    }
+}
